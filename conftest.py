2693bebfd065cd2e8ca8d2cb4535b38c68e2e525
Javier Perez Acimas <javier.perez@luceit.es>
2021-03-21 11:08:09 +0000

    Se actualizó: conftest.py

--- conftest.py
+++ conftest.py
@@ -15,7 +15,7 @@ def config() -> yaml:
     @return: YAML object
     """
     root_dir = os.path.dirname(os.path.abspath(__file__))
-    config_path = root_dir + '/config.yaml'
+    config_path = root_dir + '/data/config.yaml'
     cfg = None
     with open(config_path, 'r') as ymlfile:
         loader = yaml.Loader(ymlfile)
